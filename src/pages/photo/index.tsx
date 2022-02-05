d2a87f95d811d8a480b24fb9f3902511d308d8e0
oliver310338 <oliver310338@hotmail.com>
2022-02-05 16:51:03 +0800

    change RWD

--- src/pages/photo/index.tsx
+++ src/pages/photo/index.tsx
@@ -5,7 +5,7 @@ import Dog from "public/image/dog.jpg";
 import Food from "public/image/food.jpg";
 import Forbid from "public/image/forbid.jpg";
 import Neko from "public/image/neko.jpg";
-import Neko1 from "public/image/neko1.jpg";
+import NekoSleep from "public/image/neko_sleep.jpg";
 import Street1 from "public/image/street-1.jpg";
 import Street2 from "public/image/street-2.jpg";
 import Street3 from "public/image/street-3.jpg";
@@ -19,24 +19,24 @@ import Whale2 from "public/image/whale2.jpg";
 const Photography: NextPage = () => (
   <Layout title="攝影 | Oliver雜貨鋪">
     <div className="photo-component mt-32 flex flex-wrap flex-row px-1">
-      <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
+      <div className="grow-[50%] max-w-[50%] lg:grow-[25%] lg:max-w-[25%] px-1">
         <Image src={Street1} width={500} height={334} objectFit="contain" />
-        <Image src={Neko1} width={500} height={334} objectFit="contain" />
+        <Image src={NekoSleep} width={500} height={334} objectFit="contain" />
         <Image src={Street2} width={500} height={320} objectFit="contain" />
         <Image src={Dog} width={500} height={334} objectFit="contain" />
       </div>
-      <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
+      <div className="grow-[50%] max-w-[50%] lg:grow-[25%] lg:max-w-[25%] px-1">
         <Image src={Whale2} width={500} height={338} objectFit="contain" />
         <Image src={Street3} width={500} height={334} objectFit="contain" />
         <Image src={Forbid} width={500} height={313} objectFit="contain" />
       </div>
-      <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
+      <div className="grow-[50%] max-w-[50%] lg:grow-[25%] lg:max-w-[25%] px-1">
         <Image src={Neko} width={500} height={334} objectFit="contain" />
         <Image src={Street5} width={500} height={334} objectFit="contain" />
         <Image src={Tree} width={500} height={222} objectFit="contain" />
         <Image src={Food} width={500} height={311} objectFit="contain" />
       </div>
-      <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
+      <div className="grow-[50%] max-w-[50%] lg:grow-[25%] lg:max-w-[25%] px-1">
         <Image src={Street6} width={500} height={295} objectFit="contain" />
         <Image src={Town} width={500} height={259} objectFit="contain" />
         <Image src={Whale1} width={500} height={334} objectFit="contain" />

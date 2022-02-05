426bf1a7e2ca08e9c287a30fc9e87d9313729c48
oliver310338 <oliver310338@hotmail.com>
2022-02-05 16:46:08 +0800

    change pic scale

--- src/pages/photo/index.tsx
+++ src/pages/photo/index.tsx
@@ -20,26 +20,26 @@ const Photography: NextPage = () => (
   <Layout title="攝影 | Oliver雜貨鋪">
     <div className="photo-component mt-32 flex flex-wrap flex-row px-1">
       <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
-        <Image src={Street1} width={500} height={350} objectFit="contain" />
-        <Image src={Neko1} width={500} height={350} objectFit="contain" />
-        <Image src={Street2} width={500} height={350} objectFit="contain" />
-        <Image src={Dog} width={500} height={350} objectFit="contain" />
+        <Image src={Street1} width={500} height={334} objectFit="contain" />
+        <Image src={Neko1} width={500} height={334} objectFit="contain" />
+        <Image src={Street2} width={500} height={320} objectFit="contain" />
+        <Image src={Dog} width={500} height={334} objectFit="contain" />
       </div>
       <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
-        <Image src={Whale2} width={500} height={350} objectFit="contain" />
-        <Image src={Street3} width={500} height={350} objectFit="contain" />
-        <Image src={Forbid} width={500} height={350} objectFit="contain" />
+        <Image src={Whale2} width={500} height={338} objectFit="contain" />
+        <Image src={Street3} width={500} height={334} objectFit="contain" />
+        <Image src={Forbid} width={500} height={313} objectFit="contain" />
       </div>
       <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
-        <Image src={Neko} width={500} height={350} objectFit="contain" />
-        <Image src={Street5} width={500} height={350} objectFit="contain" />
-        <Image src={Tree} width={500} height={350} objectFit="contain" />
-        <Image src={Food} width={500} height={350} objectFit="contain" />
+        <Image src={Neko} width={500} height={334} objectFit="contain" />
+        <Image src={Street5} width={500} height={334} objectFit="contain" />
+        <Image src={Tree} width={500} height={222} objectFit="contain" />
+        <Image src={Food} width={500} height={311} objectFit="contain" />
       </div>
       <div className="grow-[50%] max-w-[50%] sm:grow-[25%] sm:max-w-[25%] px-1">
-        <Image src={Street6} width={500} height={350} objectFit="contain" />
-        <Image src={Town} width={500} height={350} objectFit="contain" />
-        <Image src={Whale1} width={500} height={350} objectFit="contain" />
+        <Image src={Street6} width={500} height={295} objectFit="contain" />
+        <Image src={Town} width={500} height={259} objectFit="contain" />
+        <Image src={Whale1} width={500} height={334} objectFit="contain" />
       </div>
     </div>
   </Layout>
